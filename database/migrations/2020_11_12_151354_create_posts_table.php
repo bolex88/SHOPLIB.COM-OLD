f1142afa9eb3ba886bce5e4d1ba2f0b4fe9252e3
James Wilson <jwilsonkla@gmail.com>
2020-12-02 11:53:55 +0000

    added database file to project

---
+++ database/migrations/2020_11_12_151354_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->longText('product_description');
+            $table->string('product_price');
+            $table->text('seller_name');
+            $table->string('address');
+            $table->longText('seller_contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
